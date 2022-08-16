6e58bd19f2aafd82d5bfc4bb739c31a93506e9a0
27MH <mohammadahamad10@gmail.com>
2022-08-16 14:04:44 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,301 @@
+let countriesData;
+let perPage = 10;
+let where = 0;
+let Border = [];
+let switchDirection = 0;
+let direction = "asc";
+function myDisplayer(some, numPerPage = 10, wheree = 0) {
+  let index = 0;
+  let counter = 0;
+  let data = JSON.parse(some);
+  let pages = data.length / numPerPage;
+  let pageStr;
+  let perPageStr;
+  countriesData = data;
+  where = wheree;
+  let section = wheree * numPerPage;
+  ///handle 20 case
+  if (data.length % numPerPage !== 0) {
+    pages += 1;
+  }
+
+  pageStr =
+    '<button onclick="myDisplayer(JSON.stringify(countriesData),perPage,' +
+    (where - 1) +
+    ')"><<</button>';
+
+  let myTable =
+    '<table class="tab" id="myTab"> \n\
+                                <tr>\n\
+                                <th><p>Save to <br> local Storage</p></th>\n\
+                                <th onclick="sortTable(2,countriesData)">Name</th>\n\
+                                <th onclick="sortTable(3,countriesData)">alpha3Code</th>\n\
+                                <th onclick="sortTable(4,countriesData)">population</th>\n\
+                                <th onclick="sortTable(5,countriesData)">Capital</th>\n\
+                                <th>Show Borders</th>\n\
+                                </tr><tr>';
+
+  //LOOP THROUGH ARRAY & GENERATE ROWS-CELLS
+
+  for (let x in data) {
+    x = Number(x) + section;
+    if (counter < numPerPage && x !== data.length) {
+      let Capital = data[x].capital;
+      if (Capital === undefined) {
+        Capital = "unKnown";
+        data[x].capital = Capital;
+      }
+
+      myTable += `<td><input type="checkbox"/></td>`;
+      myTable += `<td>${data[x].name.common}</td>`;
+      myTable += `<td>${data[x].cca3}</td>`;
+      myTable += `<td>${data[x].population}</td>`;
+      myTable += `<td>${Capital}</td>`;
+      myTable +=
+        '<td><button onclick="showBorders(countriesData[' +
+        Number(x) +
+        '])">BORDERS</button></td>';
+
+      // goto next row
+      let next = index + 1;
+      if (next !== data.length) {
+        myTable += "</tr><tr>";
+      }
+      counter++;
+      index++;
+    } else {
+      break;
+    }
+  }
+
+  //close table
+  myTable += "</table>";
+  // //ATTACH HTML TO CONTAINER
+  document.getElementById("container").innerHTML = myTable;
+  /////
+
+  ////pagination;
+  for (let pagenum = 1; pagenum <= pages; pagenum++) {
+    pageStr +=
+      '<button onclick="myDisplayer(JSON.stringify(countriesData),perPage,' +
+      (pagenum - 1) +
+      ')">' +
+      pagenum +
+      "</button>";
+  }
+  pageStr +=
+    '<button onclick="myDisplayer(JSON.stringify(countriesData),perPage,' +
+    (where + 1) +
+    ')">>></button>';
+  document.getElementById("pages").innerHTML = pageStr;
+
+  // add pagination buttons and set the click action for them
+  document.getElementById("perPage").innerHTML =
+    '#Per Page :\n\
+  <button onclick="pagination(10, countriesData)">10</button>\n\
+  <button onclick="pagination(20, countriesData)">20</button>\n\
+  <button onclick="pagination(50, countriesData)">50</button>';
+}
+
+//to choose #of countries per page
+function pagination(num, arr) {
+  where = 0;
+  perPage = num;
+  myDisplayer(JSON.stringify(arr), perPage, where);
+}
+
+function getFile(myCallback) {
+  let req = new XMLHttpRequest();
+  req.open("GET", "https://restcountries.com/v3.1/all");
+  req.onload = function () {
+    if (req.status == 200) {
+      myCallback(this.responseText);
+    } else {
+      myCallback("Error: " + req.status);
+    }
+  };
+  req.send();
+}
+
+getFile(myDisplayer);
+
+function sortTable(columnNum, arr) {
+  let jsonn;
+  if (switchDirection % 2 === 0) {
+    direction = "asc";
+    switchDirection++;
+  } else {
+    direction = "desc";
+    switchDirection++;
+  }
+  switch (columnNum) {
+    case 2:
+      if (direction === "asc") {
+        arr.sort((a, b) => {
+          let fa = a.name.common;
+          let fb = b.name.common;
+
+          if (fa < fb) {
+            return -1;
+          }
+          if (fa > fb) {
+            return 1;
+          }
+          return 0;
+        });
+      } else {
+        arr.sort((a, b) => {
+          let fa = a.name.common;
+          let fb = b.name.common;
+
+          if (fa < fb) {
+            return 1;
+          }
+          if (fa > fb) {
+            return -1;
+          }
+          return 0;
+        });
+        // arr.reverse();
+      }
+      break;
+
+    case 3:
+      if (direction === "asc") {
+        arr.sort((a, b) => {
+          let fa = a.cca3;
+          let fb = b.cca3;
+
+          if (fa < fb) {
+            return -1;
+          }
+          if (fa > fb) {
+            return 1;
+          }
+          return 0;
+        });
+      } else {
+        arr.sort((a, b) => {
+          let fa = a.cca3;
+          let fb = b.cca3;
+
+          if (fa < fb) {
+            return 1;
+          }
+          if (fa > fb) {
+            return -1;
+          }
+          return 0;
+        });
+        // arr.reverse();
+      }
+      break;
+
+    case 4:
+      if (direction === "asc") {
+        arr.sort((a, b) => {
+          return a.population - b.population;
+        });
+      } else {
+        arr.sort((a, b) => {
+          return b.population - a.population;
+        });
+        // arr.reverse();
+      }
+      break;
+    case 5:
+      if (direction === "asc") {
+        arr.sort((a, b) => {
+          let fa = a.capital;
+          let fb = b.capital;
+
+          if (fa < fb) {
+            return -1;
+          }
+          if (fa > fb) {
+            return 1;
+          }
+          return 0;
+        });
+      } else {
+        arr.sort((a, b) => {
+          let fa = a.capital;
+          let fb = b.capital;
+
+          if (fa < fb) {
+            return 1;
+          }
+          if (fa > fb) {
+            return -1;
+          }
+          return 0;
+        });
+        // arr.reverse();
+      }
+      break;
+
+    default:
+      break;
+  }
+
+  jsonn = JSON.stringify(arr);
+  myDisplayer(jsonn, perPage, where);
+}
+
+///////////////////
+
+//filtering
+
+function filtering() {
+  let input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toLowerCase();
+  table = document.getElementById("myTab");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows and coloumns to filter data
+  for (i = 0; i < tr.length; i++) {
+    td1 = tr[i].getElementsByTagName("td")[1];
+    td2 = tr[i].getElementsByTagName("td")[2];
+    td3 = tr[i].getElementsByTagName("td")[3];
+    td4 = tr[i].getElementsByTagName("td")[4];
+
+    if (td1 || td2 || td3 || td4) {
+      txtValue1 = td1.innerText;
+      txtValue2 = td2.innerText;
+      txtValue3 = td3.innerText;
+      txtValue4 = td4.innerText;
+      if (
+        txtValue1.toLowerCase().indexOf(filter) > -1 ||
+        txtValue2.toLowerCase().indexOf(filter) > -1 ||
+        txtValue3.toLowerCase().indexOf(filter) > -1 ||
+        txtValue4.toLowerCase().indexOf(filter) > -1
+      ) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+
+//borders
+function showBorders(states) {
+  if (states.borders === undefined) {
+    alert("NO borders found");
+  } else {
+    
+    alert("Borders are : " + states.borders);
+  }
+}
+
+// function searchBordersStates(border){
+//   let str="";
+//   for (let i ; i<border.length;i++){
+//     let index =array.indexOf(border.borders[i]);
+//     console.log(index+"=");
+//     str+=border[index].name.common;
+//     console.log(str+"+");
+//   }
+//   return str;
+// }
